651ccfed27cbe01d290cefe66b8e326b7a5f0dcc
daniemc <daniel.meneses27@gmail.com>
2018-06-21 11:37:16 -0500

    add map object

---
+++ src/main/java/co/com/s4n/semillero/deliveries/domain/app/Map.java
@@ -0,0 +1,11 @@
+package co.com.s4n.semillero.deliveries.domain.app;
+
+public class Map {
+    public int x;
+    public int y;
+
+    public Map(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
