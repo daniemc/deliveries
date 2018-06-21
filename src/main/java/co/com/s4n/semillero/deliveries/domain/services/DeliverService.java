24622f6bfa9d558ce0d14464a6e0398530d38a4d
Daniel MC <daniel.meneses27@gmail.com>
2018-06-21 02:47:05 -0500

    get deliver list

--- src/main/java/co/com/s4n/semillero/deliveries/domain/services/DeliverService.java
+++ src/main/java/co/com/s4n/semillero/deliveries/domain/services/DeliverService.java
@@ -1,21 +1,38 @@
 package co.com.s4n.semillero.deliveries.domain.services;
 
 import co.com.s4n.semillero.deliveries.domain.app.*;
+import io.vavr.collection.Iterator;
+import io.vavr.control.Try;
 
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import io.vavr.collection.List;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class DeliverService {
 
-    public static void createDeliveries(){
-        String fileName = "in.txt";
-        ClassLoader classLoader = DeliverService.class.getClassLoader();
-        try (Stream<String> stream = Files.lines(Paths.get(classLoader.getResource(fileName).getFile()))) {
-            stream.forEach(System.out::println);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    private static List<String> deliveriesList(){
+        Try<List<String>> deliveriesList =
+                Try.of(() -> (FileService.readDeliveriesFile()))
+                .recoverWith(Exception.class, Try.of(() -> {
+                    List<String> defaultStream = List.empty();
+                    return defaultStream;
+                }))
+                .map(list -> list);
+
+        return deliveriesList.get();
     }
+
+    private static Iterator<List<String>> partitionDeliveries(List<String> list, int size) {
+
+        return list.grouped(size);
+
+    }
+
+    public static Iterator<List<String>> createDeliveries(){
+        List<String> initialDeliveriesList = DeliverService.deliveriesList();
+        return  DeliverService.partitionDeliveries(initialDeliveriesList, 3);
+    }
+
 }
