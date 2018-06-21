973fe14dc5c852bbfeacb5055eb20f4c1b10d21c
Daniel MC <daniel.meneses27@gmail.com>
2018-06-21 02:47:16 -0500

    add test to deliver list

--- src/test/java/co/com/s4n/semillero/delivers/DeliveryServiceTest.java
+++ src/test/java/co/com/s4n/semillero/delivers/DeliveryServiceTest.java
@@ -1,7 +1,14 @@
 package co.com.s4n.semillero.delivers;
 
+
 import co.com.s4n.semillero.deliveries.domain.services.DeliverService;
+import co.com.s4n.semillero.deliveries.domain.services.FileService;
+import co.com.s4n.semillero.deliveries.domain.services.MainService;
+import io.vavr.collection.Iterator;
+import io.vavr.collection.List;
+import io.vavr.control.Try;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 
@@ -9,6 +16,20 @@ public class DeliveryServiceTest {
 
     @Test
     public void readFileTest(){
-        DeliverService.createDeliveries();
+        Try<List<String>> deliveriesList = Try.of(() -> FileService.readDeliveriesFile());
+        assertTrue(deliveriesList.isSuccess());
+    }
+
+    @Test
+    public void getDeliveriesListTest(){
+        Iterator<List<String>> deliveriesList = DeliverService.createDeliveries();
+        deliveriesList.forEach(list -> {
+            System.out.println(list);
+        });
+    }
+
+    @Test
+    public void main(){
+        MainService.deliveries();
     }
 }
