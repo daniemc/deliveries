af6bf5771cbdcbee8b6cb194ac410cba78df8362
daniemc <daniel.meneses27@gmail.com>
2018-06-21 11:54:19 -0500

    change class name map to city and add to deliver contructor

--- src/main/java/co/com/s4n/semillero/deliveries/domain/app/City.java
+++ src/main/java/co/com/s4n/semillero/deliveries/domain/app/City.java
@@ -1,12 +1,12 @@
 package co.com.s4n.semillero.deliveries.domain.app;
 
-public class Map {
+public class City {
     public int nLimit;
     public int sLimit;
     public int eLimit;
     public int oLimit;
 
-    public Map(int nLimit, int sLimit, int eLimit, int oLimit){
+    public City(int nLimit, int sLimit, int eLimit, int oLimit){
         this.nLimit = nLimit;
         this.sLimit = sLimit;
         this.eLimit = eLimit;
--- src/main/java/co/com/s4n/semillero/deliveries/domain/services/MainService.java
+++ src/main/java/co/com/s4n/semillero/deliveries/domain/services/MainService.java
@@ -1,6 +1,7 @@
 package co.com.s4n.semillero.deliveries.domain.services;
 
 import co.com.s4n.semillero.deliveries.domain.app.Drone;
+import co.com.s4n.semillero.deliveries.domain.app.City;
 import io.vavr.collection.Iterator;
 import io.vavr.collection.List;
 import io.vavr.control.Try;
@@ -13,6 +14,7 @@ public class MainService {
     public static void deliveries(){
         Iterator<List<String>> deliveriesList = DeliverService.createDeliveries();
         AtomicInteger deliveryCount = new AtomicInteger(1);
+        City city = new City(10, 10, -10, -10);
         deliveriesList.forEach(delivery -> {
             Drone drone = DroneService.prepareDroneToDelivery(delivery);
             Try.of(() -> FileService.writeDeliveryMessage("Starts delivery: " + deliveryCount.getAndIncrement() + " ---"));
